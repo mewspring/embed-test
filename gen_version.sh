ed5d43bd25a1badcfbb209a446811b941cdb371c
Robin Eklind <rnd0x00@gmail.com>
2020-12-19 02:36:22 +0100

    add gen_version.sh and use Go embed to initialize version variable

---
+++ gen_version.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git describe > version_tag_and_commit.txt
+git log -1 --format="%at" | xargs -I '{}' date -d '@{}' '+%Y-%m-%d' > version_date.txt
+printf "%s (%s)\n" "$(cat version_tag_and_commit.txt)" "$(cat version_date.txt)" > version.txt
--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
 )
 
+//go:generate ./gen_version.sh
+
+//go:embed version.txt
+var version = "unknown version"
+
 func main() {
-	fmt.Println("hello world!")
+	fmt.Println("version:", version)
 }
